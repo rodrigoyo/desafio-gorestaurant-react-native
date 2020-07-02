668b8b4a1965f50dafde87394aeab3639eadf56b
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-07-02 08:45:39 -0300

    feat: Show foods in dashboard and the details

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -55,11 +55,19 @@ const Dashboard: React.FC = () => {
 
   async function handleNavigate(id: number): Promise<void> {
     // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      const { data } = await api.get<Food[]>('foods', {
+        params: {
+          name_like: searchValue,
+          category_like: selectedCategory,
+        },
+      });
+
+      setFoods(data);
     }
 
     loadFoods();
@@ -68,6 +76,8 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadCategories(): Promise<void> {
       // Load categories from API
+      const { data } = await api.get<Category[]>('/categories');
+      setCategories(data);
     }
 
     loadCategories();
@@ -75,6 +85,9 @@ const Dashboard: React.FC = () => {
 
   function handleSelectCategory(id: number): void {
     // Select / deselect category
+    selectedCategory === id
+      ? setSelectedCategory(undefined)
+      : setSelectedCategory(id);
   }
 
   return (
